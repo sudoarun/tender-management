2c3a79daa76e83925590ad08f58203d7dfefe020
sudoarun <arunrajorya1999@gmail.com>
2024-07-09 11:30:47 +0530

    added mdbootstrap and firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as mdb from "mdb-ui-kit";
+Window = mdb;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
